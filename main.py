fd666f928a17ebe392e6e5cd526df7f0e5aba4d1
Christian Lehnert <darkroom.boy07@protonmail.com>
2022-02-13 10:20:11 +0100

    basic structure

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
+import yaml
+
 if __name__ == '__main__':
-    pass
+    # open file
+    with open("emails.yaml") as f:
+        try:
+            data = yaml.load(f, Loader=yaml.Loader)
+        except:
+            print("Email file not found")
+        else:
+            # go over every account
+
+            for account in data:
+                name = account['name']
+                email = account['email']
+                password = account['password']
+                smtp = account['smtp']
+                imap = account['imap']
+
+                # check if smtp is defined
+                if smtp is None:
+                    print(F"smtp must be defined for: {email}")
+                    continue
+
+                # create smtp request
+
